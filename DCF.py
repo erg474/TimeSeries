cdb3687a0b67a1c524c94190460e2c7d889574fa
euan.gethin <euan.gethin@ecotricity.co.uk>
2022-06-28 14:57:14 +0100

    Commit LSTM Long Short Term Memory

--- DCF.py
+++ DCF.py
@@ -1,6 +1,7 @@
 #Intrinsic stock evaluation, Discounted Cash Flow Models
 #DCF Models are a way to calculate the intrinsic value of a company
 #This can tell you whether or not a Company is overvalued
+#https://medium.com/swlh/intrinsic-valuation-of-stocks-using-python-5d902a34b1a0
 
 import numpy as np
 import pandas as pd
@@ -10,4 +11,6 @@ sales = pd.Series(index=years)
 sales['2019A'] = 15
 print(sales)
 
-#testing 1 2 3
+nwc_percent = 0.24
+nwc = sales * nwc_percent
+change
---
+++ LSTM.py
@@ -0,0 +1,5 @@
+# LSTM - Long Short Term Memory
+# ML Stock Models don't often work is due to poor implementation/understanding
+# LSTMs are one of the most widely used and successful ML financial algorithm
+# when paired with the correct problems are datasets
+
