708d0372420ab53b27797e7f1d10f948247ca272
euan.gethin <euan.gethin@ecotricity.co.uk>
2022-06-28 13:36:09 +0100

    Added DCF.py for Discounted Cash Flow Model

---
+++ DCF.py
@@ -0,0 +1,12 @@
+#Intrinsic stock evaluation, Discounted Cash Flow Models
+#DCF Models are a way to calculate the intrinsic value of a company
+#This can tell you whether or not a Company is overvalued
+
+import numpy as np
+import pandas as pd
+
+years=['2019A', '2020F', '2021F', '2022F', '2023F', '2024F']
+sales = pd.Series(index=years)
+sales['2019A'] = 15
+print(sales)
+
