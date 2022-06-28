080cd0f1e4b40e91d9e40ed8b42caa5013ec48ce
euan.gethin <euan.gethin@ecotricity.co.uk>
2022-06-28 10:54:43 +0100

    Added Monte Carlo Test of 1000 Random Walks

--- MonteCarloTest.py
+++ MonteCarloTest.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
+import finplot as fplt
 
 
 # Initialising Stock Data
@@ -15,6 +16,9 @@ stock_data_close = stock_data['Close']
 plt.plot(stock_data_close)
 plt.title("MSFT (Microsoft) Stock between 2011 and 2021")
 plt.ylabel("Price")
+df_msft = yf.download('MSFT', '2011-01-01', '2021-01-01')
+fplt.candlestick_ochl(df_msft[['Open', 'Close', 'High', 'Low']])
+fplt.show()
 
 plt.show()
 
@@ -44,4 +48,29 @@ plt.plot(price_list)
 plt.title("Random Walk From Last MSFT Price")
 plt.ylabel("Price")
 plt.xlabel("Trading Days over a year")
+plt.show()
+
+#Monte Carlo Simulation
+num_simulations = 1000
+df = pd.DataFrame()
+last_price_list = []
+for x in range(num_simulations):
+    count = 0
+    price_list = []
+    price = last_price * (1 + np.random.normal(0, daily_vol))
+    price_list.append(price) #initial price
+
+    for y in range(T):
+        price = price_list[count] * (1 + np.random.normal(0, daily_vol))
+        price_list.append(price)
+        count += 1
+
+    df[x] = price_list #creates a df column (or row?) for each Random Walk
+    last_price_list.append(price_list[-1])
+
+fig = plt.figure()
+fig.suptitle("Monte Carlo Simulation: MSFT")
+plt.plot(df)
+plt.xlabel("Day")
+plt.ylabel("Price")
 plt.show()
